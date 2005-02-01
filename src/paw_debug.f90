eba3107a58c9cda29fc048e9c01f5903f22c7720
ptpb <ptpb@svn>
2005-02-01 19:48:49 +0100

    r74 | ptpb | 2005-02-01 19:48:49 +0100 (Di, 01. Feb 2005) | 2 Zeilen Geänderte Pfade:    M /trunk/src/paw_debug.f90
    Routine to compare wave functions on a parallel computer included

--- src/paw_debug.f90
+++ src/paw_debug.f90
@@ -302,3 +302,85 @@ end subroutine paw$debug_C8
 !     **********************************************************************************
       val(:)=nint(val(:)/tol)*tol
       end subroutine debug$round_r8
+!
+!     ................................................................................
+      subroutine debug$wavefunctions()
+      use waves_module
+      USE debug_module
+      IMPLICIT NONE
+      COMPLEX(8)  ,ALLOCATABLE:: PSIG(:,:)
+      COMPLEX(8)  ,ALLOCATABLE:: PSI1(:,:)
+      INTEGER(4)              :: NTASKS,THISTASK
+      INTEGER(4)              :: IKPT,ISPIN,IB,IDIM,IWAVE
+      INTEGER(4)              :: NG1,NGG,NGL,NBH,NB
+      REAL(8)                 :: XK(3)
+      REAL(8)     ,ALLOCATABLE:: GVECL(:,:)
+      REAL(8)     ,ALLOCATABLE:: GVECG(:,:)
+      REAL(8)                 :: GBAS(3,3)
+      INTEGER(4)              :: NREC1,ISVAR
+      CHARACTER(8)            :: KEY
+      REAL(8)                 :: RBAS(3,3)
+      REAL(8)                 :: a,b
+      INTEGER(4)  ,ALLOCATABLE:: IGVECG(:,:)
+      INTEGER(4)  ,ALLOCATABLE:: IGVECL(:,:)
+      INTEGER(4)              :: ig
+      character(64)           :: comment='wave function printout'
+      real(8)      ,parameter :: tol=1.d-12
+!     **********************************************************************************
+      CALL MPE$QUERY(NTASKS,THISTASK)
+print*,'Anfang debug$wavefunctions',thistask
+  if(thistask.eq.1) then
+     OPEN (dunit, FILE=trim(adjustl(filename)), STATUS="UNKNOWN", &
+          &ACCESS="SEQUENTIAL", ACTION="WRITE", POSITION="APPEND")
+     WRITE (UNIT=dunit, FMT="(3A)", ADVANCE="YES" )"==============",comment,"==============" 
+  end if
+      DO IKPT=1,NKPT
+        CALL WAVES_SELECTWV(IKPT,1)
+        CALL PLANEWAVE$SELECT(GSET%ID)
+        NGL=GSET%NGL
+        NBH=THIS%NBH
+        NB=THIS%NB
+        CALL PLANEWAVE$GETI4('NGG',NGG)
+        ALLOCATE(IGVECL(3,NGL))
+        CALL PLANEWAVE$GETI4A('IGVEC',3*NGL,IGVECL)
+        ALLOCATE(IGVECG(3,NGG))
+        CALL PLANEWAVE$COLLECTI4(3,NGL,IGVECL,NGG,IGVECG)
+        DEALLOCATE(IGVECL)
+        ALLOCATE(PSIG(NGG,NDIM))
+        ALLOCATE(PSI1(NGL,NDIM))
+        DO ISPIN=1,NSPIN
+          CALL WAVES_SELECTWV(IKPT,ISPIN)
+          CALL PLANEWAVE$SELECT(GSET%ID)
+          DO IB=1,NBH
+            DO IDIM=1,NDIM
+              PSI1(:,IDIM)=THIS%Hpsi(:,IDIM,IB)  !<<<<=================================
+              do ig=1,ngl
+                a=real(psi1(ig,idim))
+                b=aimag(psi1(ig,idim))
+                a=tol*real(nint(a/tol),kind=8)
+                b=tol*real(nint(b/tol),kind=8)
+                psi1(ig,idim)=cmplx(a,b,kind=8)
+              enddo
+            ENDDO
+            DO IDIM=1,NDIM
+              CALL PLANEWAVE$COLLECTC8(1,NGL,PSI1(1,IDIM),NGG,PSIG(1,IDIM))
+            ENDDO
+            if(thistask.eq.1) then
+              do ig=1,ngg
+                write(dunit,*)ib,igvecg(:,ig),psig(ig,:)
+              ENDDO
+            end if
+          ENDDO
+          DEALLOCATE(PSI1)
+          DEALLOCATE(PSIG)
+        ENDDO 
+        DEALLOCATE(IGVECG)
+      enddo
+      if(thistask.eq.1) then
+        close(dunit)
+      end if
+print*,'Ende debug$wavefunctions',thistask
+      call mpe$sync
+      call error$normalstop
+      end subroutine debug$wavefunctions
+
