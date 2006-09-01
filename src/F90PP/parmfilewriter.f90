0b0faea4cde960ac7d477eab9fdc2eb16ddc1824
ptpb <ptpb@svn>
2006-09-01 11:54:18 +0200

    r438 | ptpb | 2006-09-01 11:54:18 +0200 (Fr, 01. Sep 2006) | 2 Zeilen Geänderte Pfade:    M /branches/devel_blo/src/F90PP/parmfilewriter.f90
    parmfilewriter breaks too long lines

--- src/F90PP/parmfilewriter.f90
+++ src/F90PP/parmfilewriter.f90
@@ -1,21 +1,27 @@
        program main
 !      ** program reads an input file and produces a fortran subroutine      **
 !      ** which prints the input file to a specified fortran channel         **
-       character(256) :: line
+       integer,parameter  :: maxlen=77
+       integer,parameter  :: linelen=512
+       integer,parameter  :: overheadlen=25
+       character(linelen):: line
+       character(maxlen) :: lineout
+       integer           :: len
 !      **************************************************************************
        write(*,fmt='(A)')'subroutine version$writeparmfile()'
-!       write(*,fmt='(A)')'integer(4),intent(in) :: nfil'
        do
          read(*,fmt='(A)',err=100,end=100)line
          if(len_trim(line).eq.0) cycle
          do i=1,256
            if(iachar(line(i:i)).eq.39)line(i:i)='"'
          enddo
-         if(len_trim(line).gt.256-24) then
-           stop 'error in writeparmfile: string too large'
-         end if
-         line="write(*,fmt='(A)')'"//trim(line)//"'"
-         write(*,fmt='(A)')trim(line)
+         len=len_trim(line)
+         do while (len.gt.0)
+           lineout="write(*,fmt='(A)')'"//trim(line(1:maxlen-overheadlen))//"'"
+           write(*,fmt='(A)')trim(lineout)
+           line=line(maxlen-overheadlen+1:len)
+           len=len_trim(line)
+         enddo
        enddo  
 100    continue
        write(*,fmt='(A)')'return'
